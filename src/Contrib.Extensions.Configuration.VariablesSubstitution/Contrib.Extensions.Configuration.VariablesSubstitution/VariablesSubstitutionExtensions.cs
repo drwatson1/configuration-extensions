2120fd5226f073bd73e047f683a59babebf5f62f
Sergey Tregub <sergey.tregub@vocord.ru>
2020-06-28 10:43:12 +0300

    Code style

--- src/Contrib.Extensions.Configuration.VariablesSubstitution/Contrib.Extensions.Configuration.VariablesSubstitution/VariablesSubstitutionExtensions.cs
+++ src/Contrib.Extensions.Configuration.VariablesSubstitution/Contrib.Extensions.Configuration.VariablesSubstitution/VariablesSubstitutionExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
-using System;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("VariableSubstitution.Tests")]
